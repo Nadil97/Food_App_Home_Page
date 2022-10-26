bbf9ab615b743415ac6d8c9f4f39633a34a17336
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-10-26 23:17:36 +0530

    start create body

--- food_app/lib/screens/home_screen/home_screen.dart
+++ food_app/lib/screens/home_screen/home_screen.dart
@@ -8,19 +8,45 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       drawer: Drawer(),
       appBar: AppBar(
-        title: Text('Home'),
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          'Home',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 17,
+          ),
+        ),
         actions: [
           CircleAvatar(
             radius: 12,
             backgroundColor: Color.fromARGB(252, 221, 243, 18),
+            child: Icon(
+              Icons.search,
+              size: 17,
+              color: Colors.black,
+            ),
           ),
-          CircleAvatar(
-            radius: 12,
-            backgroundColor: Color.fromARGB(252, 221, 243, 18),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: CircleAvatar(
+              radius: 12,
+              backgroundColor: Color.fromARGB(252, 221, 243, 18),
+              child: Icon(
+                Icons.shop,
+                size: 17,
+                color: Colors.black,
+              ),
+            ),
           ),
         ],
         backgroundColor: Color.fromARGB(255, 107, 107, 2),
       ),
+      body: Column(children: [
+        Container(
+          height: 150,
+          color: Colors.red,
+        ),
+      ]),
     );
   }
 }
